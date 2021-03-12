1574a64ba4f929172c35edc8c4c5648d6359de72
Manuel Silenzi <manuelsilenzi1@gmail.com>
2021-03-12 13:30:46 -0300

    crear secion añadir

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
   declare(strict_types = 1);
   require_once 'includes/app.php';
+
+  $site_title = 'KindleUpdater';
   include_once 'includes/layout/header.php';
 ?>
 
-<!-- CODE... -->
+<main class="section container container--md">
+  <div class="añadir">
+    <h2 class="fw--300">Añadir un libro</h2>
+  </div>
+</main>
 
 <?php
   include_once 'includes/layout/footer.php';
