ccb35aea4c205a60432b1da42340a7f91e8f2414
Manuel Silenzi <manuelsilenzi1@gmail.com>
2021-03-12 18:59:25 -0300

    crear enlace hacia crear.php

--- index.php
+++ index.php
@@ -6,138 +6,141 @@
   include_once 'includes/layout/header.php';
 ?>
 
-<div class="container container container--lg">
-  <section class="listado section">
-    <h2>Libros</h2>
+<section class="listado section container container--lg">
+  <h2>Libros</h2>
 
-    <div class="input-group">
-      <input
-        class="input--text listado__buscar"
-        type="text"
-        placeholder="Buscar Libros..."
-      >
-    </div>
+  <div class="input-group">
+    <input
+      class="input--text listado__buscar"
+      type="text"
+      placeholder="Buscar Libros..."
+    >
+  </div>
 
+  <div class="listado__header-table">
     <div class="listado__total">
       <div class="total__num-container">
         <p class="total__numero shadow-text">3</p>
       </div>
       <p class="total__text">Libros</p>
     </div>
+    <a href="crear.php" class="btn--transparent">
+      Añadir un libro
+    </a>
+  </div>
 
-    <div class="table-container">
-      <table class="listado__table">
-        <thead>
-          <th class="shadow-text">Nombre</th>
-          <th class="shadow-text">Autor</th>
-          <th class="shadow-text">Acciones</th>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Metro 2033</td>
-            <td>El ruso ese</td>
-            <td class="table__acciones">
-              <div class="acciones__container">
-                <a 
-                  class="table__accion"
-                  href="#" 
+  <div class="table-container">
+    <table class="listado__table">
+      <thead>
+        <th class="shadow-text">Nombre</th>
+        <th class="shadow-text">Autor</th>
+        <th class="shadow-text">Acciones</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Metro 2033</td>
+          <td>El ruso ese</td>
+          <td class="table__acciones">
+            <div class="acciones__container">
+              <a 
+                class="table__accion"
+                href="#" 
+              >
+                <img 
+                  src="src/assets/images/icons/pen.svg" 
+                  alt="Actualizar"
                 >
-                  <img 
-                    src="src/assets/images/icons/pen.svg" 
-                    alt="Actualizar"
-                  >
-                </a>
-                <form class="table__accion" method="post">
-                  <input
-                    name="id"
-                    type="hidden"
-                    value="#"
-                  >
-                  <input type="submit" value=''>
-                </form>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>Metro 2033</td>
-            <td>El ruso ese</td>
-            <td class="table__acciones">
-              <div class="acciones__container">
-                <a 
-                  class="table__accion"
-                  href="#" 
+              </a>
+              <form class="table__accion" method="post">
+                <input
+                  name="id"
+                  type="hidden"
+                  value="#"
                 >
-                  <img 
-                    src="src/assets/images/icons/pen.svg" 
-                    alt="Actualizar"
-                  >
-                </a>
-                <form class="table__accion" method="post">
-                  <input
-                    name="id"
-                    type="hidden"
-                    value="#"
-                  >
-                  <input type="submit" value=''>
-                </form>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>Metro 2033</td>
-            <td>El ruso ese</td>
-            <td class="table__acciones">
-              <div class="acciones__container">
-                <a 
-                  class="table__accion"
-                  href="#" 
+                <input type="submit" value=''>
+              </form>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>Metro 2033</td>
+          <td>El ruso ese</td>
+          <td class="table__acciones">
+            <div class="acciones__container">
+              <a 
+                class="table__accion"
+                href="#" 
+              >
+                <img 
+                  src="src/assets/images/icons/pen.svg" 
+                  alt="Actualizar"
                 >
-                  <img 
-                    src="src/assets/images/icons/pen.svg" 
-                    alt="Actualizar"
-                  >
-                </a>
-                <form class="table__accion" method="post">
-                  <input
-                    name="id"
-                    type="hidden"
-                    value="#"
-                  >
-                  <input type="submit" value=''>
-                </form>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>Metro 2033</td>
-            <td>El ruso ese</td>
-            <td class="table__acciones">
-              <div class="acciones__container">
-                <a 
-                  class="table__accion"
-                  href="#" 
+              </a>
+              <form class="table__accion" method="post">
+                <input
+                  name="id"
+                  type="hidden"
+                  value="#"
                 >
-                  <img 
-                    src="src/assets/images/icons/pen.svg" 
-                    alt="Actualizar"
-                  >
-                </a>
-                <form class="table__accion" method="post">
-                  <input
-                    name="id"
-                    type="hidden"
-                    value="#"
-                  >
-                  <input type="submit" value=''>
-                </form>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </section>
-</div>
+                <input type="submit" value=''>
+              </form>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>Metro 2033</td>
+          <td>El ruso ese</td>
+          <td class="table__acciones">
+            <div class="acciones__container">
+              <a 
+                class="table__accion"
+                href="#" 
+              >
+                <img 
+                  src="src/assets/images/icons/pen.svg" 
+                  alt="Actualizar"
+                >
+              </a>
+              <form class="table__accion" method="post">
+                <input
+                  name="id"
+                  type="hidden"
+                  value="#"
+                >
+                <input type="submit" value=''>
+              </form>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>Metro 2033</td>
+          <td>El ruso ese</td>
+          <td class="table__acciones">
+            <div class="acciones__container">
+              <a 
+                class="table__accion"
+                href="#" 
+              >
+                <img 
+                  src="src/assets/images/icons/pen.svg" 
+                  alt="Actualizar"
+                >
+              </a>
+              <form class="table__accion" method="post">
+                <input
+                  name="id"
+                  type="hidden"
+                  value="#"
+                >
+                <input type="submit" value=''>
+              </form>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</section>
 
 <?php
   include_once 'includes/layout/footer.php';
