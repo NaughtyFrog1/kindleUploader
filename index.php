03633c5100d228927b73c1dc54111c105ac70945
Manuel Silenzi <manuelsilenzi1@gmail.com>
2021-03-12 18:23:07 -0300

    crear tabla listado libros

--- index.php
+++ index.php
@@ -82,7 +82,118 @@
       </div>
       <p class="total__text">Libros</p>
     </div>
-    
+
+    <div class="table-container">
+      <table class="listado__table">
+        <thead>
+          <th class="shadow-text">Nombre</th>
+          <th class="shadow-text">Autor</th>
+          <th class="shadow-text">Acciones</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Metro 2033</td>
+            <td>El ruso ese</td>
+            <td class="table__acciones">
+              <div class="acciones__container">
+                <a 
+                  class="table__accion"
+                  href="#" 
+                >
+                  <img 
+                    src="src/assets/images/icons/pen.svg" 
+                    alt="Actualizar"
+                  >
+                </a>
+                <form class="table__accion" method="post">
+                  <input
+                    name="id"
+                    type="hidden"
+                    value="#"
+                  >
+                  <input type="submit" value=''>
+                </form>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Metro 2033</td>
+            <td>El ruso ese</td>
+            <td class="table__acciones">
+              <div class="acciones__container">
+                <a 
+                  class="table__accion"
+                  href="#" 
+                >
+                  <img 
+                    src="src/assets/images/icons/pen.svg" 
+                    alt="Actualizar"
+                  >
+                </a>
+                <form class="table__accion" method="post">
+                  <input
+                    name="id"
+                    type="hidden"
+                    value="#"
+                  >
+                  <input type="submit" value=''>
+                </form>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Metro 2033</td>
+            <td>El ruso ese</td>
+            <td class="table__acciones">
+              <div class="acciones__container">
+                <a 
+                  class="table__accion"
+                  href="#" 
+                >
+                  <img 
+                    src="src/assets/images/icons/pen.svg" 
+                    alt="Actualizar"
+                  >
+                </a>
+                <form class="table__accion" method="post">
+                  <input
+                    name="id"
+                    type="hidden"
+                    value="#"
+                  >
+                  <input type="submit" value=''>
+                </form>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Metro 2033</td>
+            <td>El ruso ese</td>
+            <td class="table__acciones">
+              <div class="acciones__container">
+                <a 
+                  class="table__accion"
+                  href="#" 
+                >
+                  <img 
+                    src="src/assets/images/icons/pen.svg" 
+                    alt="Actualizar"
+                  >
+                </a>
+                <form class="table__accion" method="post">
+                  <input
+                    name="id"
+                    type="hidden"
+                    value="#"
+                  >
+                  <input type="submit" value=''>
+                </form>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </section>
 </div>
 
