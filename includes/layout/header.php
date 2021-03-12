e970c4aca3ff3ab079fce75db0d368b368db2125
Manuel Silenzi <manuelsilenzi1@gmail.com>
2021-03-12 11:37:48 -0300

    agregar fuentes google fonts

--- includes/layout/header.php
+++ includes/layout/header.php
@@ -8,6 +8,12 @@
     content=""
   >
   <title><?= $site_title ?></title>
+
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link 
+    href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Poppins:wght@300;400;700&display=swap" 
+    rel="stylesheet"
+  >
   <link rel="stylesheet" href="<?= ROOT_DIR ?>build/css/main.min.css">
 </head>
 <body <?= ($site_id ? "id='{$site_id}'" : '') ?> >
